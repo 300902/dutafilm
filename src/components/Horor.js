0b0a9b95a51514b922f0c3dc5f1b92d284a59284
ridwan <ridwan11depok@gmail.com>
2024-06-04 15:45:42 +0700

    horror

--- src/components/Horor.js
+++ src/components/Horor.js
@@ -1,114 +1,158 @@
-import { Card, Container, Row, Col, Image } from 'react-bootstrap';
-import mangkujiwoImage from '../assets/image/horor/it.jpg';
-import danurImage from '../assets/image/horor/danur.jpg';
-import ghibahImage from '../assets/image/horor/ghibah.jpg';
-import penjamuaniblisImage from '../assets/image/horor/midsommar.jpg';
-import sewudinoImage from '../assets/image/horor/exorcist.jpg';
-import siksakuburImage from '../assets/image/horor/conjuring.jpg';
-
+import { Card, Container, Row, Col, Image } from "react-bootstrap";
+import mangkujiwoImage from "../assets/image/horor/it.jpg";
+import danurImage from "../assets/image/horor/danur.jpg";
+import ghibahImage from "../assets/image/horor/ghibah.jpg";
+import penjamuaniblisImage from "../assets/image/horor/midsommar.jpg";
+import sewudinoImage from "../assets/image/horor/exorcist.jpg";
+import siksakuburImage from "../assets/image/horor/conjuring.jpg";
 
 const Horor = () => {
-    return (
-        <div>
-            <Container >
-                <br />
-                <h1 className="text-white">Horror Movies</h1>
-                <br />  
-                <Row>
-                    <Col md={4} className="movieWrapper" id="horror">
-                        <Card className="movieImage">
-                            <Image src={mangkujiwoImage} alt="Mangkujiwo Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Mangkujiwo</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4}className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={danurImage} alt="Danur Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Danur</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={ghibahImage} alt="Ghibah Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Ghibah</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={penjamuaniblisImage} alt="Penjamuaniblis Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Penjamuan Iblis</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={sewudinoImage} alt="Sewudino Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Sewu Dino</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={siksakuburImage} alt="Siksakubur Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Siksa Kubur</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Container>
+        <br />
+        <h1 className="text-white">Horror Movies</h1>
+        <br />
+        <Row>
+          <Col md={4} className="movieWrapper" id="horror">
+            <Card className="movieImage">
+              <Image
+                src={mangkujiwoImage}
+                alt="Mangkujiwo Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">It Chapter 2</Card.Title>
+                  <Card.Text className="text-left">
+                    Seri kedua akan melanjutkan kisah para anggota Losers Club.
+                    Dua puluh tujuh tahun setelah pertemuan pertama mereka
+                    dengan Pennywise yang menakutkan, Losers Club telah dewasa
+                    dan berpisah. Sampai sebuah panggilan telepon membawa Losers
+                    Club kembali untuk bertemu hantu kelam dari masa lalu
+                    mereka.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image src={danurImage} alt="Danur Movies" className="images" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Hereditary</Card.Title>
+                  <Card.Text className="text-left">
+                    Ketika ibu dari keluarga Graham meninggal, keluarga anak
+                    perempuannya mulai dihantui oleh rahasia yang misterius dan
+                    menakutkan tentang nenek moyang mereka. Semakin mereka
+                    berusaha mencari tahu, semakin mereka terancam oleh takdir
+                    yang mengerikan ini.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image src={ghibahImage} alt="Ghibah Movies" className="images" />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Insidious</Card.Title>
+                  <Card.Text className="text-left">
+                    Josh dan dan Renai Lambert pindah ke sebuah mansion baru
+                    bersama ke-tiga anak mereka. Keadaan berubah mengerikan
+                    ketika Dalton dan anak mereka, terjatuh dan koma setelah
+                    serangan misterius di loteng.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={penjamuaniblisImage}
+                alt="Penjamuaniblis Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Midsommar</Card.Title>
+                  <Card.Text className="text-left">
+                    Saat hubungan keduanya tengah bermasalah, sepasang kekasih
+                    muda dari Amerika Serikat melancong ke sebuah festival
+                    tengah musim panas Swedia, di mana tempat yang tampak
+                    seperti surga permai berubah menjadi mimpi buruk yang
+                    mengerikan, seiring warga setempat mulai menunjukkan niat
+                    bengis mereka.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={sewudinoImage}
+                alt="Sewudino Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">The Exorcist</Card.Title>
+                  <Card.Text className="text-left">
+                    Kepercayaan Merrin pada Tuhan tertebus setelah dia pergi ke
+                    Afrika Timur untuk mengekskavasi sebuah gereja. Seorang
+                    bocah lelaki yang dirasuki iblis menjadikan satu-satunya
+                    jalan terang baginya.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={siksakuburImage}
+                alt="Siksakubur Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">The Conjuring 2</Card.Title>
+                  <Card.Text className="text-left">
+                    Ketika suami istri Rod dan Carolyn menyadari bahwa keluarga
+                    mereka tengah diganggu oleh roh jahat - keduanya pun
+                    akhirnya meminta bantuan pada pasangan ahli supranatural
+                    untuk menyelidiki masalah ini.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Horor;
--- src/components/Superhero.js
+++ src/components/Superhero.js
@@ -1,114 +1,170 @@
-import { Card, Container, Row, Col, Image } from 'react-bootstrap';
-import antmanImage from '../assets/image/super hero/deadpool.jpg';
-import avengersImage from '../assets/image/super hero/avengers.jpg';
-import garudaImage from '../assets/image/super hero/Garuda.jpeg';
-import shangciImage from '../assets/image/super hero/ShangCi.jpg';
-import spidermanImage from '../assets/image/super hero/Spiderman.jpg';
-import sriasihImage from '../assets/image/super hero/ironman.jpg';
-
+import { Card, Container, Row, Col, Image } from "react-bootstrap";
+import antmanImage from "../assets/image/super hero/deadpool.jpg";
+import avengersImage from "../assets/image/super hero/avengers.jpg";
+import garudaImage from "../assets/image/super hero/Garuda.jpeg";
+import shangciImage from "../assets/image/super hero/ShangCi.jpg";
+import spidermanImage from "../assets/image/super hero/Spiderman.jpg";
+import sriasihImage from "../assets/image/super hero/ironman.jpg";
 
 const SuperHero = () => {
-    return (
-        <div>
-            <Container >
-                <br />
-                <h1 className="text-white">SuperHero Movies</h1>
-                <br />  
-                <Row>
-                    <Col md={4} className="movieWrapper" id="superhero">
-                        <Card className="movieImage">
-                            <Image src={antmanImage} alt="Agaklaen Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Ant Man</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4}className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={avengersImage} alt="Agaklaen Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Avengers</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={garudaImage} alt="Agaklaen Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Garuda</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={shangciImage} alt="Agaklaen Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Shang Ci</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={spidermanImage} alt="Agaklaen Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Spiderman</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={4} className="movieWrapper">
-                        <Card className="movieImage">
-                            <Image src={sriasihImage} alt="Agaklaen Movies" className="images" />
-                            <div className="bg-dark">
-                                <div className="p-2 m-1 text-white">
-                                    <Card.Title className="text-center">Sri Asih</Card.Title>
-                                    <Card.Text className="text-left">
-                                    This is a wider card with supporting text below as a natural lead-in
-                                    to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                    <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                                </div>
-                            </div>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Container>
+        <br />
+        <h1 className="text-white">SuperHero Movies</h1>
+        <br />
+        <Row>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage">
+              <Image
+                src={antmanImage}
+                alt="Agaklaen Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Dead Pool</Card.Title>
+                  <Card.Text className="text-left">
+                    Wade Wilson (Ryan Reynolds) berusaha melindungi seorang
+                    mutan misterius yang diincar Cable (Josh Brolin). Untuk
+                    melindunginya, Wade lalu membentuk sebuah tim bernama
+                    X-Force yang beranggotakan Deadpool sendiri, Domino,
+                    Negasonic Teenage Warhead, Colossus, dan Bedlam.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={avengersImage}
+                alt="Agaklaen Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">
+                    Avengers Endgame
+                  </Card.Title>
+                  <Card.Text className="text-left">
+                    Melanjutkan Avengers Infinity War, dimana kejadian setelah
+                    Thanos berhasil mendapatkan semua infinity stones dan
+                    memusnahkan 50% semua mahluk hidup di alam semesta. Akankah
+                    para Avengers berhasil mengalahkan Thanos?
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={garudaImage}
+                alt="Agaklaen Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Shazam</Card.Title>
+                  <Card.Text className="text-left">
+                    Billy Batson, seorang anak yatim berusia 14 tahun yang
+                    bermasalah yang tinggal di Philadelphia, diatur untuk pindah
+                    ke rumah asuh baru - yang ketujuh berturut-turut - dengan
+                    keluarga Vazquez dan lima anak asuh lainnya. Suatu hari,
+                    Billy naik mobil subway dan menemukan dirinya diangkut ke
+                    dunia yang berbeda di mana penyihir kuno memberinya
+                    kekuatan...
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={shangciImage}
+                alt="Agaklaen Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Shang Ci</Card.Title>
+                  <Card.Text className="text-left">
+                    Shang-Chi (Simu Liu) harus menghadapi masa lalunya sebelum
+                    ia memilih untuk meninggalkan dan bergabung ke dalam sebuah
+                    organisasi bernama Ten Rings.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={spidermanImage}
+                alt="Agaklaen Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Spiderman</Card.Title>
+                  <Card.Text className="text-left">
+                    Peter Parker beraksi memerangi kejahatan sebagai sosok
+                    Spider-Man di Manhattan. Oscorp, milik sahabat masa
+                    kecilnya, Harry Osborn, mulai melepaskan musuh-musuh
+                    rekayasa genetika untuk menyerangnya.
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage">
+              <Image
+                src={sriasihImage}
+                alt="Agaklaen Movies"
+                className="images"
+              />
+              <div className="bg-dark">
+                <div className="p-2 m-1 text-white">
+                  <Card.Title className="text-center">Iron Man 3</Card.Title>
+                  <Card.Text className="text-left">
+                    Industrialis brilian Tony Stark / Iron Man menghadapi musuh
+                    yang tak kenal batas. Dunia Tony dihancurkan dan ia
+                    menyelidiki siapa yang bertanggung jawab untuk kerusakan
+                    tersebut. Berbekal peralatan dan instingnya, Tony berusaha
+                    menyelamatkan orang-orang yang dikasihinya. Pertanyaan lama
+                    kembali menghantuinya; apakah manusia yang membuat baju Iron
+                    Man atau baju tersebut yang menentukan identitasnya sebagai
+                    manusia?
+                  </Card.Text>
+                  <Card.Text className="text-left">
+                    Last updated 3 mins ago
+                  </Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default SuperHero;
